2e1d1ab97410f0cc383f37c0d4ac24774104eac4
Ogaga <iyaraogaga69@gmail.com>
2022-08-02 07:52:08 +0100

    Modify item.rb file

--- lib/item.rb
+++ lib/item.rb
@@ -1,22 +1,37 @@
-require 'securerandom'
 require 'date'
 
 class Item
-  attr_accessor :date, :author, :label, :source, :genre
+  attr_reader :id, :archived, :label, :genre, :author
+  attr_accessor :publish_date
 
-  def initialize(date, archived)
-    @id = SecureRandom.uuid
-    @publish_date = date
+  def initialize(publish_date, archived: false, id: rand(1..1000))
+    @id = id
+    @publish_date = Date.strptime(publish_date, '%Y-%m-%d')
     @archived = archived
   end
 
-  def can_be_archived?
-    return true if Date.parse(published_date).year.to_i - Date.today.year.to_i > 10
-
-    false
+  def move_to_archive
+    @archived = true if can_be_archived?
   end
 
-  def move_to_archive()
-    @archived = true if can_be_archived?
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  private
+
+  def can_be_archived?
+    Time.now.year - @publish_date.year > 10
   end
 end
