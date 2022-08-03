cfced31422f8e213097cb2f80632681a760401cb
Chia Carlyle <chiacarlyle@gmail.com>
2022-08-03 17:50:29 +0100

    Write tests for the Label class

---
+++ Rspec/label_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../lib/label.rb'
+require_relative '../lib/item.rb'
+
+describe Label do
+  before :each do
+    @label = Label.new 'Title', 'Color'
+  end
+
+  describe '#new' do
+    it 'takes two parameters and returns a Label object' do
+      expect(@label).to be_an_instance_of Label
+    end
+  end
+
+  describe '#title' do
+    it 'detects the title of label' do
+      expect(@label.title).to eq 'Title'
+    end
+  end
+
+  describe '#color' do
+    it 'detects the color of label' do
+      expect(@label.color).to eq 'Color'
+    end
+  end
+  it 'should detect the label items' do
+    book = Book.new 'name', 'Publisher', 'Cover state', '2011-03-28'
+    @label.add_items(book)
+    expect(@label.items.include?(book)).to eq true
+    expect(book.label).to eq @label
+  end
+end
