1fe3fc6d90a5d41580ca69ff5d081b86e5ee1174
Assad Isah <nottherealalanturing@gmail.com>
2022-08-03 17:51:16 +0100

    fix: resolve linter errors with rubocop

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,15 +1,14 @@
 class Genre
-    attr_accessor :name
+  attr_accessor :name
 
-    def initialize(name)
-        @name = name
-        @id = id: rand(1..1000)
-        @items = []
-    end
+  def initialize(name)
+    @name = name
+    @id = rand(1..1000)
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.add_genre(self)
-    end
-
+  def add_item(item)
+    @items << item
+    item.add_genre(self)
+  end
 end
--- lib/music_album.rb
+++ lib/music_album.rb
@@ -1,15 +1,14 @@
-require "./item"
+require './item'
 
 class MusicAlbum < Item
-    def initialize(on_sportify)
-        @on_sportify = on_sportify
-        super
-    end
+  def initialize(on_sportify)
+    @on_sportify = on_sportify
+    super
+  end
 
-    def can_be_archived?
-        return true if super && @on_sportify
+  def can_be_archived?
+    return true if super && @on_sportify
 
-        false
-    end
+    false
+  end
 end
-
