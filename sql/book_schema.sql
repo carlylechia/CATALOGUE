a6214747ac07ddb75c3dbb375f228275bdeb8233
Ogaga <iyaraogaga69@gmail.com>
2022-08-04 07:39:51 +0100

    Create authors table

--- sql/book_schema.sql
+++ sql/book_schema.sql
@@ -17,3 +17,11 @@ CREATE TABLE book(
   FOREIGN KEY (label_id) REFERENCES label(id),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE authors (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+);
+
+
