f4afa8d81dc1f229b44c9050dd3a0de82cb46c70
Assad Isah <nottherealalanturing@gmail.com>
2022-08-04 15:05:20 +0100

    test: add test for Music Album class

--- Rspec/genre_spec.rb
+++ Rspec/genre_spec.rb
@@ -2,12 +2,11 @@ require_relative '../lib/genre'
 require_relative '../lib/item'
 
 RSpec.describe Genre do
-    before(:all) do
-      @genre = Genre.new('Comedy')
-    end
+  before(:all) do
+    @genre = Genre.new('Comedy')
+  end
 
-    context 'when a new Genre is created' do
-
+  context 'when a new Genre is created' do
     it 'should be an instance of Genre' do
       expect(@genre).to be_instance_of Genre
     end
@@ -15,6 +14,5 @@ RSpec.describe Genre do
     it 'should have a name attribute' do
       expect(@genre).to have_attributes(name: 'Comedy')
     end
-
   end
 end
